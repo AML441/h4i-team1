ed99230f0ba4af7a1f94b6266318f660c47bb9a5
AL441 <amberl8701@gmail.com>
2025-04-14 21:29:43 -0400

    setting up!

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_tm6lcWoBSu6WKzwQ9pA39LEP6yj-OgA",
+  authDomain: "smiski-angel.firebaseapp.com",
+  projectId: "smiski-angel",
+  storageBucket: "smiski-angel.firebasestorage.app",
+  messagingSenderId: "661828352717",
+  appId: "1:661828352717:web:cf904459bfb4de1bda4784",
+  measurementId: "G-G8BBPRQ43Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
