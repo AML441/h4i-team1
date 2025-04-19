e411847e15533153e0453babf35e4b6dbdc73ac4
AL441 <amberl8701@gmail.com>
2025-04-19 00:23:58 -0400

    rough drafting client profile page

---
+++ src/pages/ClientProfilePage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ClientProfilePage() {
+    return (
+        <div>
+            {/* box for list of products */}
+            {/* Will need to set up the routing so that I can test if the dimensions are right*/}
+            <div className="mx-auto mt-auto w-[10rem] bg-[#C7A2D8] border-2" >
+                
+
+
+            </div>
+
+
+        </div>
+
+    );
+
+};
+
+export default ClientProfilePage
