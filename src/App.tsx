84396b7158db06702856d46e003685545e59366e
AL441 <amberl8701@gmail.com>
2025-04-24 14:53:22 -0400

    re-adding client profile page to the routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,11 @@ import ClientProductCatalogue from "./pages/ClientProductCatalogue";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import "./App.css"
 import ForgotPassword from "./pages/ForgotPassword";
-import Register from "./pages/RegisterPage";
+import RegisterPage from "./pages/RegisterPage";
 import VendorProductCatalogue from "./pages/VendorProductCatalogue";
 import ClientCartCheckedOut from "./pages/ClientCartCheckedOut";
 import VendorProductPage from "./pages/VendorProductPage";
-import ClientCart from "./pages/ClientCart";
-
-
-
+import ClientProfilePage from "./pages/ClientProfilePage";
 
 function App() {
     const [user, setUser] = useState<User | null>(null);
@@ -26,25 +23,19 @@ function App() {
     }, []);
   
     return (
-        <>
-            <div>
-            <h1 className="bg-blue-500"> Testing</h1>
-            </div>
-        <BrowserRouter>
-            <Routes>
-            <Route path="/" element={user ? <ClientProductCatalogue user={user}/> : <Navigate to="login" replace/>} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/vendor-catalogue" element={<VendorProductCatalogue />} />
-            <Route path="/checked-out" element={<ClientCartCheckedOut />} />
-            <Route path="/vendor-products" element={<VendorProductPage />} />
-            <Route path="/cart" element={<ClientCart />} />
-
-
-            </Routes>
-        </BrowserRouter>
-      </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={user ? <ClientProductCatalogue user={user}/> : <Navigate to="login" replace/>} />
+          {/*I think there's an error here because due to ClientProduct Page not passing in a user prop yet - Amber*/}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/vendor-catalogue" element={<VendorProductCatalogue />} />
+          <Route path="/checkout-out" element={<ClientCartCheckedOut />} />
+          <Route path="/vendor-products" element={<VendorProductPage />} />
+          <Route path="/client-profile" element={<ClientProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     );
 }
 
